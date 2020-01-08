ac5b8488f67633b837b42d5bf5a4a880033850a4
ByteTheBit1 <skourtsman@hotmail.com>
2020-01-09 00:54:25 +0200

    v.1 url's fixed

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const ResolutionCodeModel = require('../models/ResolutionCodeModel');
 const MongoClient = require("mongodb").MongoClient;
 
 
