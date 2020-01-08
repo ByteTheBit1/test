6d614df17447dc32c835fcdf00fd1c10c59bb537
ByteTheBit1 <skourtsman@hotmail.com>
2020-01-08 04:29:39 +0200

    1a,b,c leipei to optional param

--- routes/ActualTotalLoad.js
+++ routes/ActualTotalLoad.js
@@ -1,22 +1,36 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var router = express.Router();
-var assert = require('assert')
+const router = express.Router();
+const assert = require('assert')
 const URL = 'mongodb+srv://user:user@cluster0-0pwss.mongodb.net/test?retryWrites=true&w=majority'
 const MongoClient = require('mongodb').MongoClient
-//const CircularJSON = require('circular-json');
-//const {parse, stringify} = require('flatted/cjs');
 
 
+             /*        1a       */
+// Ta sxolia sto dateObj einai peiramatismos gia to current date
+// Gia Optional Metablites vazoume erotimatiko ? sto telos tou onomatos tous,alla meta 
+// pagideuetai edo pera to querry kai den paei sta alla get
+// Logika mporoume na kanoume mono ena get me tetoio styl:
 
-// Erotima 1a
+/* get('/:AreaName/:Resolution/:Year?/:Month?/:Day?', (req,res,next) =>{
+      edo pera kanoume to 1a kai meta kanoume next() etsi oste na pame sto 1b ktlp
+})
+*/
+
+// Erotima 1a    
 router.get('/:AreaName/:Resolution/:Year/:Month/:Day', (req, res) => {
-  const _AreaName=req.params.AreaName
-  const _Resolution=req.params.Resolution
-  const _Year = parseInt(req.params.Year)
-  const _Month = parseInt(req.params.Month)
-  const _Day = parseInt(req.params.Day)
+
+    let _AreaName = req.params.AreaName
+    let _Resolution=req.params.Resolution
+    //let dateObj = new Date();
+    let _Year = parseInt(req.params.Year)   //|| dateObj.getUTCFullYear();
+    let _Month = parseInt(req.params.Month) //|| dateObj.getUTCMonth() + 1; //months from 1-12
+    let _Day = parseInt(req.params.Day)     //|| dateObj.getUTCDate();
+
+     console.log(_Year,_Month,_Day) // -> Checks for DATE Value
+
+ 
 
   MongoClient.connect(URL,{
     useNewUrlParser: true,
@@ -26,7 +40,7 @@ router.get('/:AreaName/:Resolution/:Year/:Month/:Day', (req, res) => {
       else console.log('connected to db');
       assert.equal(null, err) 
       const db = client.db('energy')
-      var collection = db.collection('ActualTotalLoad')
+      let collection = db.collection('ActualTotalLoad')
       const agg = [
         { 
           $match : 
@@ -101,7 +115,7 @@ router.get('/:AreaName/:Resolution/:Year/:Month/:Day', (req, res) => {
           }
         }
       ];
-      var cursor = collection.aggregate(agg)
+      let cursor = collection.aggregate(agg)
 
       await cursor.toArray((error, result) => {
         if(error) {
@@ -133,79 +147,71 @@ router.get('/:AreaName/:Resolution/:Year/:Month', (req, res) => {
       else console.log('connected to db');
       assert.equal(null, err) 
       const db = client.db('energy')
-      var collection = db.collection('ActualTotalLoad')
-      const agg = [
-        {
-          $project :{
-            _id:0,
-            Source :'entso-e',
-            Dataset :'ActualTotalLoad',
-            AreaName: '$AreaName',
-            AreaTypeCode: '$Area_Type_Code.AreaTypeCodeText',
-            MapCode:'$Map_Code.MapCodeText',
-            ResolutionCode : '$resolution_codes.ResolutionCodeText',
-            Year : '$Year',
-            Month : '$Month',
-            Day : '$Day',
-            DateTimeUTC: '$DateTime',
-            ActualTotalLoadValue:"$TotalLoadValue",
-            UpdateTimeUTC: '$UpdateTime'
+      let collection = db.collection('ActualTotalLoad')
+      const agg =
+    [
+      {$match: {
+        AreaName: _AreaName,
+        Month: _Month,
+        Year: _Year
+      }}, {$group: {
+        _id: {
+          Day:"$Day",
+          Year:"$Year",
+          Month:"$Month", 
+          AreaName: "$AreaName",
+          AreaTypeCodeId:"$AreaTypeCodeId",
+          AreaCodeId:"$AreaCodeId",
+          ResolutionCodeId:"$ResolutionCodeId",
+          MapCodeId:"$MapCodeId"
+        },
+        ActualTotalLoadPerDay: {
+          $sum: "$TotalLoadValue"
+        },
+      }}, {$lookup: {
+            'from': 'ResolutionCode', 
+            'localField': '_id.ResolutionCodeId', 
+            'foreignField': 'Id', 
+            'as': 'resolution_codes'
+          }}, {$unwind: {
+        path: '$resolution_codes'
+      }}, {$match: {
+        'resolution_codes.ResolutionCodeText': _Resolution
+      }}, {$lookup: {
+        from: 'MapCode',
+        localField: '_id.MapCodeId',
+        foreignField: 'Id',
+        as: 'Map_Code'
+      }}, {$unwind: {
+        path: '$Map_Code'
+      }}, {$lookup: {
+        from: 'AreaTypeCode',
+        localField: '_id.AreaTypeCodeId',
+        foreignField: 'Id',
+        as: 'Area_Type_Code'
+      }}, {$unwind: {
+      
+      
+            path:'$Area_Type_Code'
+      
+      
+          }}, {$project: {
+        _id:0,
+        Source: 'entso-e',
+        Dataset: 'ActualTotalLoad',
+        AreaName: '$_id.AreaName',
+        AreaTypeCode: '$Area_Type_Code.AreaTypeCodeText',
+        MapCode: '$Map_Code.MapCodeText',
+        ResolutionCode: '$resolution_codes.ResolutionCodeText',
+        Year: { $toString:'$_id.Year'},
+        Month: { $toString:'$_id.Month'},
+        Day: { $toString:'$_id.Day'},
+        ActualTotalLoadByDayValue: { $toString:'$ActualTotalLoadPerDay'}
+      }}, {$sort: {
+        Day: 1
+      }}];
 
-      }},
-      { 
-        $match : 
-        {
-          AreaName: _AreaName,
-          Day : _Day,
-          Month: _Month,
-          Year: _Year
-        }
-    },
-      {$group: {
-          _id : {Year : _Year, Month : _Month, Day : _Day},
-          ActualTotalLoad : { "$sum" : "$TotalLoadValue"}
-      }},
-      {
-        $lookup:
-        {
-          from: 'ResolutionCode',
-          localField: 'ResolutionCodeId',
-          foreignField : 'Id',
-          as: "resolution_codes"
-        }
-      },
-      {
-        $unwind: {path : "$resolution_codes"}
-      },
-
-      { 
-        $match : {'resolution_codes.ResolutionCodeText' : _Resolution}
-      },
-      {
-        $lookup: {
-          from: 'MapCode', 
-          localField: 'MapCodeId', 
-          foreignField: 'Id', 
-          as : 'Map_Code'
-        }
-      }, {
-        $unwind: {
-          path: '$Map_Code'
-        }
-      }, {
-        $lookup: {
-          from: 'AreaTypeCode', 
-          localField: 'AreaTypeCodeId', 
-          foreignField: 'Id', 
-          as: 'Area_Type_Code'
-        }
-      }, {
-        $unwind: {
-          path: '$Area_Type_Code'
-        }
-      }];
-
-      var cursor = collection.aggregate(agg)
+      let cursor = collection.aggregate(agg)
 
       await cursor.toArray((error, result) => {
         if(error) {
@@ -218,14 +224,12 @@ router.get('/:AreaName/:Resolution/:Year/:Month', (req, res) => {
 })
 
 
-/*
+
 //erotima 1c
-router.get('/:AreaName/:Resolution/:Year/:Month/:Day', (req, res) => {
+router.get('/:AreaName/:Resolution/:Year/', (req, res) => {
   const _AreaName=req.params.AreaName
   const _Resolution=req.params.Resolution
   const _Year = parseInt(req.params.Year)
-  const _Month = parseInt(req.params.Month)
-  const _Day = parseInt(req.params.Day)
 
   MongoClient.connect(URL,{
     useNewUrlParser: true,
@@ -235,82 +239,65 @@ router.get('/:AreaName/:Resolution/:Year/:Month/:Day', (req, res) => {
       else console.log('connected to db');
       assert.equal(null, err) 
       const db = client.db('energy')
-      var collection = db.collection('ActualTotalLoad')
-      const agg = [
-        { 
-          $match : 
-          {
-            AreaName: _AreaName,
-            Day : _Day,
-            Month: _Month,
-            Year: _Year
-          }
-      },
-
-        {
-          $lookup:
-          {
-            from: 'ResolutionCode',
-            localField: 'ResolutionCodeId',
-            foreignField : 'Id',
-            as: "resolution_codes"
-          }
+      let collection = db.collection('ActualTotalLoad')
+      const agg = [{$match: {
+        AreaName: _AreaName,
+        Year: _Year
+      }}, {$group: {
+        _id: {
+          Year:"$Year",
+          Month:"$Month", 
+          AreaName: "$AreaName",
+          AreaTypeCodeId:"$AreaTypeCodeId",
+          AreaCodeId:"$AreaCodeId",
+          ResolutionCodeId:"$ResolutionCodeId",
+          MapCodeId:"$MapCodeId"
         },
-        {
-          $unwind: {path : "$resolution_codes"}
+        ActualTotalLoadByMonthValue: {
+          $sum: "$TotalLoadValue"
         },
-
-        { 
-          $match : {'resolution_codes.ResolutionCodeText' : _Resolution}
-        },
-        {
-          $lookup: {
-            from: 'MapCode', 
-            localField: 'MapCodeId', 
-            foreignField: 'Id', 
-            as : 'Map_Code'
-          }
-        }, {
-          $unwind: {
-            path: '$Map_Code'
-          }
-        }, {
-          $lookup: {
-            from: 'AreaTypeCode', 
-            localField: 'AreaTypeCodeId', 
-            foreignField: 'Id', 
-            as: 'Area_Type_Code'
-          }
-        }, {
-          $unwind: {
-            path: '$Area_Type_Code'
-          }
-        }, 
-        {
-          $project : 
-          {
-            _id:0,
-            Source :'entso-e',
-            Dataset :'ActualTotalLoad',
-            AreaName: '$AreaName',
-            AreaTypeCode: '$Area_Type_Code.AreaTypeCodeText',
-            MapCode:'$Map_Code.MapCodeText',
-            ResolutionCode : '$resolution_codes.ResolutionCodeText',
-            Year : '$Year',
-            Month : '$Month',
-            Day : '$Day',
-            DateTimeUTC: '$DateTime',
-            ActualTotalLoadValue:"$TotalLoadValue",
-            UpdateTimeUTC: '$UpdateTime'
-          }
-        },
-        {
-          $sort: {
-            'DateTime': 1
-          }
-        }
-      ];
-      var cursor = collection.aggregate(agg)
+      }}, {$lookup: {
+            'from': 'ResolutionCode', 
+            'localField': '_id.ResolutionCodeId', 
+            'foreignField': 'Id', 
+            'as': 'resolution_codes'
+          }}, {$unwind: {
+        path: '$resolution_codes'
+      }}, {$match: {
+        'resolution_codes.ResolutionCodeText': _Resolution
+      }}, {$lookup: {
+        from: 'MapCode',
+        localField: '_id.MapCodeId',
+        foreignField: 'Id',
+        as: 'Map_Code'
+      }}, {$unwind: {
+        path: '$Map_Code'
+      }}, {$lookup: {
+        from: 'AreaTypeCode',
+        localField: '_id.AreaTypeCodeId',
+        foreignField: 'Id',
+        as: 'Area_Type_Code'
+      }}, {$unwind: {
+      
+      
+            path:'$Area_Type_Code'
+      
+      
+          }}, {$project: {
+        _id:0,
+        Source: 'entso-e',
+        Dataset: 'ActualTotalLoad',
+        AreaName: '$_id.AreaName',
+        AreaTypeCode: '$Area_Type_Code.AreaTypeCodeText',
+        MapCode: '$Map_Code.MapCodeText',
+        ResolutionCode: '$resolution_codes.ResolutionCodeText',
+        Year: { $toString:'$_id.Year'},
+        Month: { $toString:'$_id.Month'},
+        ActualTotalLoadByMonthValue: { $toString:'$ActualTotalLoadByMonthValue'}
+      }}, {$sort: {
+        Month: 1
+      }}];
+      let cursor = collection.aggregate(agg)
 
       await cursor.toArray((error, result) => {
         if(error) {
@@ -322,5 +309,5 @@ router.get('/:AreaName/:Resolution/:Year/:Month/:Day', (req, res) => {
   })// connection ends here
 })
 
-*/
+
 module.exports = router;
--- routes/AggregatedGenerationPerType.js
+++ routes/AggregatedGenerationPerType.js
@@ -1,31 +1,130 @@
 const express = require('express');
 const router = express.Router();
 
-//GET METHOD route
-router.get('/',(req,res,next)=>{
-    req.status(200).json({
+//Erotima 2c
+router.get('/:_AreaName/:_ProductionType/:_Resolution/:_Year',(req,res,next)=>{
 
+    MongoClient.connect(URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true}, 
+        async (err, client) => {
+          if (err) throw err; 
+          else console.log('connected to db');
+          assert.equal(null, err) 
+          const db = client.db('energy')
+          var collection = db.collection('AggregatedGenerationPerType')
+
+          const agg = [{
+        $match: {
+            AreaName: req.params._AreaName,
+            Year: req.params._Year
+        }
+    }, {
+        $group: {
+            _id: {
+                Year: '$Year',
+                Month: '$Month',
+                AreaName: '$AreaName',
+                AreaTypeCodeId: '$AreaTypeCodeId',
+                AreaCodeId: '$AreaCodeId',
+                ResolutionCodeId: '$ResolutionCodeId',
+                MapCodeId: '$MapCodeId',
+                ProductionTypeId: '$ProductionTypeId'
+            },
+            ActualGenerationOutputByMonthValue: {
+                $sum: '$ActualGenerationOutput'
+            }
+        }
+    }, {
+        $lookup: {
+            from: 'ResolutionCode',
+            localField: '_id.ResolutionCodeId',
+            foreignField: 'Id',
+            as: 'resolution_codes'
+        }
+    }, {
+        $unwind: {
+            path: '$resolution_codes'
+        }
+    }, {
+        $match: {
+            'resolution_codes.ResolutionCodeText': req.params._Resolution
+        }
+    }, {
+        $lookup: {
+            from: 'ProductionType',
+            localField: '_id.ProductionTypeId',
+            foreignField: 'Id',
+            as: 'Producton_Type'
+        }
+    }, {
+        $unwind: {
+            path: '$Producton_Type'
+        }
+    }, {
+        $match:{
+            '$Producton_Type.ProductionTypeText':_ProductionType
+        }
+    },{
+        $lookup: {
+            from: 'MapCode',
+            localField: '_id.MapCodeId',
+            foreignField: 'Id',
+            as: 'Map_Code'
+        }
+    }, {
+        $unwind: {
+            path: '$Map_Code'
+        }
+    }, {
+        $lookup: {
+            from: 'AreaTypeCode',
+            localField: '_id.AreaTypeCodeId',
+            foreignField: 'Id',
+            as: 'Area_Type_Code'
+        }
+    }, {
+        $unwind: {
+            path: '$Area_Type_Code'
+        }
+    }, 
+    {
+        $project: {
+            _id: 0,
+            Source: 'entso-e',
+            Dataset: 'AggregatedGenerationPerType',
+            AreaName: '$_id.AreaName',
+            AreaTypeCode: '$Area_Type_Code.AreaTypeCodeText',
+            MapCode: '$Map_Code.MapCodeText',
+            ResolutionCode: '$resolution_codes.ResolutionCodeText',
+            Year: {
+                $toString: '$_id.Year'
+            },
+            Month: {
+                $toString: '$_id.Month'
+            },
+            ProductionType: '$Producton_Type.ProductionTypeText',
+            ActualGenerationOutputByMonthValue: {
+                $toString: '$ActualGenerationOutputByMonthValue'
+            }
+        }
+    }, {
+        $sort: {
+            Month: 1
+        }
+    }];
+
+
+    var cursor = collection.aggregate(agg)
+
+      await cursor.toArray((error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        res.send(result);
     });
-});
 
-//POST METHOD route
-router.post('/',(req,res,next)=>{
-    res.status(201).json({
-
-    });
-});
-
-// PATCH method route
-router.patch('/',  (req, res,next) =>{
-    res.status(200).json({
-  });
-});
-
-//DELETE METHOD route
-router.delete('/', (req,res,next)=>{
-    res.status(200).json({
-    });
-});
-
+  })// connection ends here
+})
 
 module.exports = router;
